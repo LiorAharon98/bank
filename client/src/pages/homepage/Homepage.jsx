999d1a41112a080ea9179c16cec3d6a29fe9cd83
LiorAharon98 <lior.ah98@gmail.com>
2023-04-21 19:59:47 +0300

    add side bar

--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
 import styles from "./homepage.module.css";
-import { useDataProvider } from "../../context/Data";
-import { useEffect } from "react";
 import Footer from "../../components/footer/Footer";
-import Flag from "react-world-flags";
+import { Animate } from "react-simple-animate";
 const Homepage = () => {
+  const test = () => {
+    let strArr = 'the quick brown fox jumps over the lazy dog.'.toLowerCase();
+    let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
+    for (let i = 0; i < alphabet.length; i++) {
+      if (strArr.indexOf(alphabet[i]) < 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+  console.log(test());
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.side_bar}></div>
+        <Animate duration={0.5} play start={{ width: 0 }} end={{ width: "50%" }}>
+          <div className={styles.side_bar}></div>
+        </Animate>
       </div>
       <Footer />
     </>
